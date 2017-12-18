84ccccf64c3b4b5a3096ac62aaf08641da487721
Nicholas Corgan <n.corgan@gmail.com>
2017-12-18 07:11:29 -0800

    Added skeleton unit test

---
+++ testing/unit-tests/attribute_engine_test.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2017 Nicholas Corgan (n.corgan@gmail.com)
+ *
+ * Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+ * or copy at http://opensource.org/licenses/MIT)
+ */
+
+#include <gtest/gtest.h>
+
+TEST(cpp_attribute_test, test_registering)
+{
+}
+
+TEST(cpp_attribute_test, test_get_attribute_names)
+{
+}
+
+TEST(cpp_attribute_test, test_valid_values)
+{
+}
